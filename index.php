ce6f964d8d6813bd46da7f098d7776b7b9645a02
potato <phughes1997@gmail.com>
2019-09-09 16:58:12 +1000

    php'ified

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-
-	<head>
+    <head>
 			<title>A Journal by Arnold Christian.</title>
 			<link rel="shortcut icon" type="image/png" href="images/favicon.png">
-			
+
 			<meta charset="utf-8">
 			<meta name="viewport" content="width=device-width, intial-scale=1.0">
 
@@ -13,99 +12,33 @@
 
 	</head>
 
-	<body>
-		
-		
-		<div id="1-10">
+<body>
+<div id="1-10">
+<article id="1">
 
-			<article id="1">
-				<h1><a href="#1">Lorem Ipsum</a></h1>
-				<h2>What is Lorem-Ipsum?</h2>
-				<time datetime="2019-09-05">Sep. 05, 2019</time>
 
-				<p>
+<?php
+$files = scandir('entries/');
+$i = 0;
+foreach($files as $file) {
+    $i++;
+    if ($i>2) {
+        $txt = file_get_contents("entries/" . $file);
+        list($a,$b,$c,$d) = explode('##',$txt);
+        $e = '<h1><a href="#1">' . $a .'</a></h1>';
+        echo $e;
+        $f = '<h2>' . $b . '</h2>';
+        echo $f;
+        $h = '<time>' . $c . '</time>';
+        echo $h;
+        $g = '<p>' . $d . '</p>';
+        echo $g;
+        }
+  }
+?>
 
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-				</p>
-				<p><?php include('myFile.txt'); ?></p>
 
-			</article>
-
-			<article id="2">
-
-				<h1><a href="#2">Journaling</a></h1>
-				<h2>the philosophy behind the hobby</h2>
-				<time datetime="2019-09-05">Sep. 05, 2019</time>
-
-				<p>
-
-					Inspired by the <a title="Meditations" href="https://en.wikipedia.org/wiki/Meditations">personal writings</a> of the Roman Emperor Marcus Aurelius, I always thought that journaling my life and thoughts could be an act comparable to meditation; and could be a way that I could release some stress. During the creation of this blog, I was conflicted about what the theme of this project would be, and it is funny that I am deciding to turn this blog into a journal, based on how the aesthetic of the website turned out to be.
-
-				</p>
-
-			</article>
-
-			<article id="3">
-			
-				<h1><a href="#3">First Journal</a></h1>
-				<h2>Let's see if it's worth it</h2>
-				<time datetime="2019-09-06">Sep. 06, 2019</time>
-
-				<p>
-
-					Journaling daily has been said to ease the symptoms of anxiety. Lately, I have been under some stress because time seems to be flying past quicker than I can keep track of; even my parents mentioned it today. I am at a major changing point in my life and it seems like I have to adapt quickly to avoid long-term suffering. However, reflecting back on this now; I think that maybe I should just take some time to calm down and learn to not take life so seriously.
-				
-				</p>
-
-			</article>
-			
-			<article id="4">
-			
-				<h1><a href="#4">Going forward</a></h1>
-				<h2>What does the future hold?</h2>
-				<time datetime="2019-09-06T22:40">Sep. 06, 2019</time>
-
-				<p>
-
-					I have always taken some sort of pride in what I do in life. The process of creation, and putting something out into the world has always been something that has given me a tremendous sense of achievement. You can see this in things I have done in the past, such as creating my own .webs.com website, or trying to learn C++ with my highschool frend so that I may create my own video games. This passion still runs deep in my soul. I still want to create something, be it a podcast, another website, a portfolio, or whatever else I can think of. I want to turn these things into a career that I can actually enjoy. I think that what I really need to do is to wait and learn different things and try as many things as I can, so that I can decide what I want to do in my own time, and enjoy the process along then way, living free of anxiety and stress, and my own compulsive nature of wanting to do everything all at once.
-				
-				</p>
-
-			</article>
-			
-			<article id="5">
-			
-				<h1><a href="#5">Tamarind</a></h1>
-				<h2>Named after the first thing I saw on my desk</h2>
-				<time datetime="2019-09-07">Sep. 07, 2019</time>
-
-				<p>
-
-					The skin on my right foot big toe is dry and cracked, making it hard to even bend my toe without feeling any pain. But that aside, for a guy who always writes about not letting anxiety about the future rule him, I spent all of today thinking even more about the future. It's not long till I finish my Bachelor's Degree and begin looking for a full time job, and this seems like a huge step in my life. No more three-month long vacations, no mid-semester breaks, no relaxing. However, I am looking forward to being able to support myself and my family once I start earning a full time income. 	
-					
-				</p>
-				
-				<p>
-				
-					It seems that as time goes on, the monetary system works even less in favour of the humans who created it. House prices are skyrocketing, while the median wage rises slowly over the years; making it incredibly difficult for some people to have access to even the most basic housing. I am fearful that I will be trapped in this system just as I see many people trapped around me today, a slave to money. Somewhere in my mind, I wish I could live a simpler life, free of these worries, the way nature intended, but I realise that it is money that improved our standard of living exponentially. Where did we go wrong? Nonetheless, I will have to put up with this no matter what my opinion is, unless a better solution comes around.
-				
-				</p>
-				
-				<p>
-				
-					I have recently heard about things such as the 'two-day rule' which encourages making a commitment to doing some sort of exercise every second day at the least. I have been neglecting my health for a while now, not leaving my house and choosing to play video-games. I heard about this concept from <a href="https://www.youtube.com/user/blackboxfilmcompany">Matt D'Avella</a> who also produced some other videos which I really liked, <a href="https://www.youtube.com/watch?v=ATDYLTnGJV0">Simple Steps to Financial Freedom</a> being my favourite, which has motivated me to start thinking about my money more wisely. I have decided to try and implement these ideas in my life, let's see how this turns out.
-				
-				</p>
-
-			</article>
-
-		</div>
-		
-		<div id="11-20">
-		
-		</div>
-	
-	
-	</body>
-
-</head>
+</article>
+</div>
+</body>
+</html>
